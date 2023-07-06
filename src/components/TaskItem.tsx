47e3b865d0cb430034ef9583c1718a48c1edca1b
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-07-06 02:21:36 +0200

    create taskItem component

---
+++ src/components/TaskItem.tsx
@@ -0,0 +1,54 @@
+﻿import { TasksContext } from '@/context/TasksContext';
+import React, { useContext } from 'react';
+
+type Props = {
+	name: string;
+	isDone: boolean;
+	index: number;
+};
+
+export default function TaskItem({ name, isDone, index }: Props) {
+	const { dispatch } = useContext(TasksContext);
+
+	const toggleTodo = (index: number) => {
+		dispatch({ id: index, type: 'TOGGLE_TODO' });
+	};
+
+	const deleteTask = (index: number) => {
+		dispatch({ type: 'DELETE_TASK', id: index });
+	};
+
+	return (
+		<li
+			className='flex p-6 justify-between items-center gap-6 bg-gray-500 rounded-xl'
+			key={index}>
+			<div
+				className={`w-8 h-8 rounded-full shrink-0 cursor-pointer transition-colors duration-300  
+						${
+							isDone
+								? 'text-gray-100 text-xl bg-purple-dark hover:bg-purple flex justify-center items-center'
+								: 'hover:border-blue-dark border-blue border-solid border'
+						}
+						`}
+				onClick={() => {
+					toggleTodo(index);
+				}}>
+				{isDone && <>&#10003;</>}
+				<input className='hidden' type='checkbox' />
+			</div>
+			<p
+				className={`text-gray-100 text-2xl ${
+					isDone && 'line-through text-gray-300'
+				}`}>
+				{name}
+			</p>
+			<button
+				className='w-[24px] h-[24px]shrink-0 text-3xl text-gray-300 hover:text-danger hover:bg-gray-400 transition-colors duration-300 rounded-lg'
+				onClick={() => {
+					deleteTask(index);
+				}}>
+				&#10007;
+			</button>
+		</li>
+	);
+}
--- src/components/TasksList.tsx
+++ src/components/TasksList.tsx
@@ -1,9 +1,10 @@
 ﻿import Image from 'next/image';
 import { TasksContext } from '@/context/TasksContext';
 import React, { useContext } from 'react';
+import TaskItem from './TaskItem';
 
 export default function TasksList() {
-	const { state: tasks, dispatch } = useContext(TasksContext);
+	const { state: tasks } = useContext(TasksContext);
 
 	const areTasks = (): boolean => (tasks.length > 0 ? true : false);
 
@@ -12,37 +13,7 @@ export default function TasksList() {
 			{areTasks() ? (
 				<ul className='flex flex-col gap-9'>
 					{tasks.map((task, index) => (
-						<li
-							className='flex p-6 justify-between items-center gap-6 bg-gray-500 rounded-xl'
-							key={index}>
-							<div
-								className={`w-8 h-8 rounded-full shrink-0 cursor-pointer transition-colors duration-300  
-						${
-							task.isDone
-								? 'text-gray-100 text-xl bg-purple-dark hover:bg-purple flex justify-center items-center'
-								: 'hover:border-blue-dark border-blue border-solid border'
-						}
-						`}
-								onClick={() => {
-									dispatch({ id: index, type: 'TOGGLE_TODO' });
-								}}>
-								{task.isDone && <>&#10003;</>}
-								<input className='hidden' type='checkbox' />
-							</div>
-							<p
-								className={`text-gray-100 text-2xl ${
-									task.isDone && 'line-through text-gray-300'
-								}`}>
-								{task.name}
-							</p>
-							<button
-								className='w-[24px] h-[24px]shrink-0 text-3xl text-gray-300 hover:text-danger hover:bg-gray-400 transition-colors duration-300 rounded-lg'
-								onClick={() => {
-									dispatch({ type: 'DELETE_TASK', id: index });
-								}}>
-								&#10007;
-							</button>
-						</li>
+						<TaskItem {...task} index={index} key={index} />
 					))}
 				</ul>
 			) : (
