bcd07b586fa07beb86b6fe662908e048d6e1906c
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-07-06 02:23:35 +0200

    create EmptyTasksList component

---
+++ src/components/EmptyTasksList.tsx
@@ -0,0 +1,20 @@
+﻿import Image from 'next/image';
+
+export default function EmptyTasksList() {
+	return (
+		<div className='flex flex-col justify-center items-center gap-6 mt-8'>
+			<Image
+				src='/assets/images/clipboard.png'
+				alt='clipboard icon'
+				width={56}
+				height={56}
+			/>
+			<p className='text-gray-300 font-bold text-3xl text-center'>
+				You don't have registered tasks yet
+			</p>
+			<p className='text-gray-300 text-3xl text-center'>
+				Create tasks and organize your to-do items
+			</p>
+		</div>
+	);
+}
--- src/components/TasksList.tsx
+++ src/components/TasksList.tsx
@@ -1,7 +1,7 @@
-﻿import Image from 'next/image';
-import { TasksContext } from '@/context/TasksContext';
+﻿import { TasksContext } from '@/context/TasksContext';
 import React, { useContext } from 'react';
 import TaskItem from './TaskItem';
+import EmptyTasksList from './EmptyTasksList';
 
 export default function TasksList() {
 	const { state: tasks } = useContext(TasksContext);
@@ -17,20 +17,7 @@ export default function TasksList() {
 					))}
 				</ul>
 			) : (
-				<div className='flex flex-col justify-center items-center gap-6 mt-8'>
-					<Image
-						src='/assets/images/clipboard.png'
-						alt='clipboard icon'
-						width={56}
-						height={56}
-					/>
-					<p className='text-gray-300 font-bold text-3xl text-center'>
-						You don't have registered tasks yet
-					</p>
-					<p className='text-gray-300 text-3xl text-center'>
-						Create tasks and organize your to-do items
-					</p>
-				</div>
+				<EmptyTasksList />
 			)}
 		</>
 	);
